e8ec4527a336e0f4086a94c06c83969e4607167e
Debangi <debangiofficial@gmail.com>
2024-06-07 14:30:14 +0530

    NATSPEC  added

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+/*
+ * @title DecentralizedStableCoin
+ * @author Debangi
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting : Algorithmic
+ * Relative Stability : Pegged to USD
+ *
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ *
+ *
+ */
+
+contract DecentralizedStableCoin {
+    constructor() {}
+}
